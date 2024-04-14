f23ca5c4b3f0c65231cb1f7891912ad19de594ea
lgonzalez-3420 <little.red.cloud@gmail.com>
2024-04-14 18:01:57 -0500

    Search By Name Is Complete

--- src/store/actions.js
+++ src/store/actions.js
@@ -2,7 +2,7 @@ import useAPI from "@/composables/useAPI";
 
 const { api } = useAPI()
 
-export function searchedMeals({ commit }, keyword) {
+export function searchMeals({ commit }, keyword) {
     api.get(`search.php?s=${keyword}`)
         .then(({ data }) => {
         commit('setSearchedMeals', data.meals)
