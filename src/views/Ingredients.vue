078f6facddaa6aff96cfce1ce18ed8da0ab71935
lgonzalez-3420 <little.red.cloud@gmail.com>
2024-04-21 18:23:38 -0500

    Meals By Ingredients Done

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -33,12 +33,13 @@ const searchIngredients = computed(() => {
         <input type="text" v-model="keyword" class="rounded border-2 border-gray-200 w-full bg-white mb-5" placeholder="Enter ingredient name" />
         
         <div class="grid grid-cols-4 gap-5">
-            <router-link v-for="ingredient in searchIngredients" :key="ingredient.idIngredient" 
+            <router-link 
+                v-for="ingredient in searchIngredients" 
+                :key="ingredient.idIngredient" 
                 :to="{name: 'MealsByIngredients', params: {ingredient: ingredient.strIngredient}}" 
                 class="bg-white rounded p-3 mb-3 shadow block">
-
-                <img :src="`http://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png`" />
-                <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
+                    <img :src="`http://www.themealdb.com/images/ingredients/${ingredient.strIngredient}-Small.png`" />
+                    <h3 class="text-2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
 
             </router-link>
         </div>
